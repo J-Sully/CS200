6b943789957ac00ac2d41eac23307878d690c234
jsullivanp96 <jessie.sully@gmail.com>
2019-07-22 15:06:21 -0700

    deleted comment

--- CS200_Exercise10_4_RedefBaseFunct/CS200_Exercise10_4_RedefBaseFunct/main.cpp
+++ CS200_Exercise10_4_RedefBaseFunct/CS200_Exercise10_4_RedefBaseFunct/main.cpp
@@ -9,12 +9,9 @@
 #include "Theater.h"
 
 int main(int argc, const char * argv[]) {
-  /*  Theater(const string &type, unsigned int year, unsigned int capacity, double basePrice,
-   string &name, string &address, double avgConcessionSales)
-   */
-  
   Venue house("private", 1965, 35, 75);
   Theater theatre("private", 1930, 200, 20, "Theatre de Musique", "5000 MacArthur Blvd, Oakland CA 94511", 2352);
+  
   house.print();
   theatre.print();
   
